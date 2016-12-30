73a71d61b66ca29275942f52d6ca00a5d760e7b7
v4ss1l <vasil.cc@abv.bg>
2016-12-30 18:55:17 +0200

    Add deadline charts

--- Client/src/app/pipes/get-pie-data.pipe.ts
+++ Client/src/app/pipes/get-pie-data.pipe.ts
@@ -4,15 +4,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class GetPieDataPipe implements PipeTransform {
     transform(task: any): any {
 
-        let createdAt = new Date(task.createdAt);
-        let timeForExecution: number = task.timeForExecution;
-        let deadline = createdAt.setHours(createdAt.getHours() + timeForExecution);
-        let now = Date.now();
+        let pieData = [];
         
-        // if () {
+        let timeForExecution: number = +task.timeForExecution;
+        let createdAt = new Date(task.createdAt);
+        let now = new Date(Date.now()); 
+        let deadline = new Date(createdAt.getTime());
+        deadline.setHours(deadline.getHours() + timeForExecution);
+        let timeSpent = now.getTime() - createdAt.getTime();
+        let timeLeft = deadline.getTime() - now.getTime();
 
-        // }
+        if (timeLeft < 1) {
+            timeLeft = 0;
+        }
+           
+        pieData.push({category: 'Time left', value: timeLeft});
+        pieData.push({category: 'Time spent', value: timeSpent});
+
+        return pieData;
     }
-
-    
 }
